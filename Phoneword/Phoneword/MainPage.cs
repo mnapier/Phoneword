cc8f86bf5bd17bdffe0ffabcfa6c5053c23ad748
mnapier <markwnapier@gmail.com>
2016-06-20 18:03:54 -0400

    Add project files.

---
+++ Phoneword/Phoneword/MainPage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public class MainPage : ContentPage
+    {
+        private Entry _phoneNumberEntry;
+        private Button _translateButton;
+        private Button _callButton;
+
+        private string _translatedNumber;
+
+        public MainPage()
+        {
+            Padding = new Thickness(20, Device.OnPlatform<double>(40, 20, 20), 20, 20);
+
+            StackLayout panel = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Orientation = StackOrientation.Vertical,
+                Spacing = 15,
+            };
+
+            panel.Children.Add(new Label
+            {
+                Text = "Enter a Phonework:",
+            });
+
+            panel.Children.Add(_phoneNumberEntry = new Entry
+            {
+                Text = "1-855-XAMARIN"
+            });
+
+            panel.Children.Add(_translateButton = new Button
+            {
+                Text = "Translate"
+            });
+
+            panel.Children.Add(_callButton = new Button
+            {
+                Text = "Call",
+                IsEnabled = false
+            });
+
+            
+            _translateButton.Clicked += OnTranslate;
+
+            Content = panel;
+        }
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = _phoneNumberEntry.Text;
+            _translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrWhiteSpace(_translatedNumber))
+            {
+                _callButton.IsEnabled = true;
+                _callButton.Text = "Call " + _translatedNumber;
+            }
+            else
+            {
+                _callButton.IsEnabled = false;
+                _callButton.Text = "Call";
+            }
+        }
+    }
+}
---
+++ Phoneword/Phoneword/PhonewordTranslator.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace Core
+{
+    public static class PhonewordTranslator
+    {
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+            {
+                return null;
+            }
+
+            raw = raw.ToUpperInvariant();
+
+            var newNumber = new StringBuilder();
+            foreach (var c in raw)
+            {
+                if (" -0123456789".Contains(c))
+                {
+                    newNumber.Append(c);
+                }
+                else
+                {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                    {
+                        newNumber.Append(result);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+
+            return newNumber.ToString();
+        }
+
+        private static bool Contains(this string keyString, char c)
+        {
+            return keyString.IndexOf(c) >= 0;
+        }
+
+        private static readonly string[] Digits =
+        {
+            "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+        };
+
+        private static int? TranslateToNumber(char c)
+        {
+            for (int i = 0; i < Digits.Length; i++)
+            {
+                if (Digits[i].Contains(c))
+                {
+                    return 2 + i;
+                }
+            }
+
+            return null;
+        }
+    }
+}
